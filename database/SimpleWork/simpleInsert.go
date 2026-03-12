1963430dc8e218476a8a193c4c9b525211c86d7f
KokosCmarsa556 <donssmit@mail.ru>
2026-03-12 13:17:00 +0500

    SimpleWorkDB\ Changed method in file 'simpleInsert.go'

--- database/SimpleWork/simpleInsert.go
+++ database/SimpleWork/simpleInsert.go
@@ -8,22 +8,24 @@ import (
 )
 
 func InsertRow(ctx context.Context, conn *pgx.Conn, student *user.User) error {
+	//Создаем запрос. Не передаем id, т.к он генерируется у нас в PostgreSQL
 	sqlInsert := `
 		INSERT INTO students (Name, First_Name, Last_Name, Gender, Address, Iin)
-		VALUES($1, $2, $3, $4, $5, $6, $7)
+		VALUES($1, $2, $3, $4, $5, $6);
 	`
 
+	// Отправляем все данные пользователя в БД
 	if _, err := conn.Exec(
 		ctx,
 		sqlInsert,
-		student.Id,
 		student.Name,
 		student.FirstName,
 		student.LastName,
 		student.Gender,
 		student.Address,
-		student.IIN); err != nil {
-		return err
+		student.IIN); err != nil { // проверяем на ошибки
+		return err // если ошибка есть, то возвращаем ее
 	}
+	// Если ошибки нет, то просто возвращаем nil
 	return nil
 }
