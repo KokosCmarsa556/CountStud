d171fd179603fef59465776697d65d387cbabfa7
KokosCmarsa556 <donssmit@mail.ru>
2026-03-12 13:04:30 +0500

    SimpleWorkDB\ Changed method 'simpleDelete.go'

--- database/SimpleWork/simpleDelete.go
+++ database/SimpleWork/simpleDelete.go
@@ -3,17 +3,30 @@ package simpleWork
 import (
 	user "CountStud/User"
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func DeleteRow(ctx context.Context, conn *pgx.Conn, u *user.User) error {
+	//Создаем запрос для удаления пользователя из БД. RETURNING id - проверяет есть ли такой пользователь или нет
 	sqlDelete := `
-		DELETE FROM students WHERE id = $1
+		DELETE FROM students WHERE id = $1 RETURNING id;
 	`
-
-	if _, err := conn.Exec(ctx, sqlDelete, u.Id); err != nil {
+	//Возвращем поличество удаленных строк и ошибку из запроса
+	cmdTag, err := conn.Exec(ctx, sqlDelete, u.Id)
+	//Проверяем на наличие ошибок
+	if err != nil {
 		return err
 	}
+	// Проверяем, что если кол-во возврщаемых строк 0, то значит, что такого пользователя нет
+	if cmdTag.RowsAffected() == 0 {
+		//Логируем ошибку
+		log.Fatal("Student not found")
+		//Возвращаем ошибку
+		return fmt.Errorf("Student not found")
+	}
+	//Если все правильно и ошибок нет, значит пользователь удален
 	return nil
 }
