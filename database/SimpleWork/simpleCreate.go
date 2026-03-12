ee7f4e07b56e0ace6b7e3e98edf31d6442404ef9
KokosCmarsa556 <donssmit@mail.ru>
2026-03-12 12:55:01 +0500

    SimpleWorkDB\ Changed request in 'simpleCreate.go' for DB

--- database/SimpleWork/simpleCreate.go
+++ database/SimpleWork/simpleCreate.go
@@ -9,14 +9,14 @@ import (
 
 func CreateTable(ctx context.Context, conn *pgx.Conn) error {
 	sqlCreate := `
-		CREATE TABLE students (
-			Id SERIAL PRIMARY KEY,
+		CREATE TABLE IF NOT EXISTS students (
+			Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 			Name VARCHAR(150) NOT NULL,
 			First_Name VARCHAR(150) NOT NULL,
 			Last_Name VARCHAR(150) NOT NULL,
 			Gender VARCHAR(50) NOT NULL,
 			Address VARCHAR(150),
-			Iin INTEGER NOT NULL
+			Iin VARCHAR(12) NOT NULL
 		)
 	`
 
