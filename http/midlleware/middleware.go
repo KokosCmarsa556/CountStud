badb62594c13488354818cee293cba8828695925
KokosCmarsa556 <donssmit@mail.ru>
2026-04-06 23:17:57 +0500

    Added cors middleware

--- http/midlleware/middleware.go
+++ http/midlleware/middleware.go
@@ -74,3 +74,24 @@ func JWTMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+
+		if origin == "http://localhost:3000" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
